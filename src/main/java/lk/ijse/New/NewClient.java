17226230b049a708f66bc63b2573e038677f4704
Your Name <you@example.com>
2024-10-13 13:37:05 +0530

    Message can send continuously

--- src/main/java/lk/ijse/New/NewClient.java
+++ src/main/java/lk/ijse/New/NewClient.java
@@ -14,21 +14,26 @@ public class NewClient {
 
 
         try {
-            System.out.println("Client Start");
-            Socket socket = new Socket("localhost",9806);
 
-            System.out.print("input Message from client : ");
-            String message = sc.nextLine();
+            while (true) {
+//                System.out.println("Client Start");
+                Socket socket = new Socket("localhost", 9806);
 
-            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-            dataOutputStream.writeUTF(message);
-            dataOutputStream.flush();
-            System.out.println("Message sent");
+                System.out.print("input Message from client : ");
+                String message = sc.nextLine();
 
-            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
-            String s = dataInputStream.readUTF();
-            System.out.println(s);
-            System.out.println("message received ");
+                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                dataOutputStream.writeUTF(message);
+                dataOutputStream.flush();
+                System.out.println("Message sent");
+
+                DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+                String s = dataInputStream.readUTF();
+                System.out.println(s);
+                System.out.println("message received ");
+                System.out.println("Message from Server : " + s);
+
+            }
 
         } catch (IOException e) {
             throw new RuntimeException(e);
--- src/main/java/lk/ijse/New/NewServer.java
+++ src/main/java/lk/ijse/New/NewServer.java
@@ -16,24 +16,27 @@ public class NewServer {
 
 
         try {
-            System.out.println("waiting for the Client ");
             ServerSocket serverSocket = new ServerSocket(9806);
-            Socket socket = serverSocket.accept();
-            System.out.println("Client connected");
 
-            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
-            String s = dataInputStream.readUTF();
-            System.out.println("Message from Client : " + s);
+            while (true) {
+                System.out.println("waiting for the Client ");
 
+                Socket socket = serverSocket.accept();
+                System.out.println("Client connected");
 
-            System.out.print("Message from Server : ");
-            String s1 = sc.nextLine();
-            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-            dataOutputStream.writeUTF(s1);
-            dataOutputStream.flush();
-            System.out.println("Message send  from server Successfully");
+                DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+                String s = dataInputStream.readUTF();
+                System.out.println("Message from Client : " + s);
 
 
+                System.out.print("Message from Server : ");
+                String s1 = sc.nextLine();
+                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                dataOutputStream.writeUTF(s1);
+                dataOutputStream.flush();
+                System.out.println("Message send  from server Successfully");
+
+            }
 
         } catch (IOException e) {
             throw new RuntimeException(e);
